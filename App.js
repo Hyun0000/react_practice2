c7bf512f4ea5e66d9d5beada10715942b254819e
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2022-03-15 22:34:00 +0900

    #2 react bootstrap을 이용한 쇼핑몰 레이아웃
    #2 react bootstrap을 이용한 쇼핑몰 레이아웃

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
+// react-bootstrap '라이브러리'에서 import 해오는 것이다.
 import './App.css';
-import { Navbar, Container, Nav, NavDropdown, Button, FormControl } from 'react-bootstrap';
+import Data, {name1 as apple, name2} from './data.js'
+// import {name1 as apple, name2} from './data.js'
 
 function App() {
+  // 상품데이터
+  let [shoes, shoesChange] = useState(Data);
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Garden_Park</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -28,36 +35,58 @@ function App() {
 
       <div className="container">
         <div className="row">
+      {
+        shoes.map(function(e, index) {
+          return (<Plist index={index} shoes={shoes}/>);
+        })
+      }
+        </div>
+      </div>  
+
+      {/* <div className="container">
+        <div className="row">
           <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="" srcset="" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="" srcSet="" width="100%"/>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content} & {shoes[0].price}</p>
           </div>
           <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="" srcset="" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="" srcSet="" width="100%"/>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content} & {shoes[1].price}</p>
           </div>
           <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="" srcset="" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="" srcSet="" width="100%"/>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].content} & {shoes[2].price}</p>
           </div>
         </div>
-      </div>
+      </div>   */}
+
     </div>
   )
 }
 
 function Jumbotron() {
   return(
-    <div className="jumboBackground">
+    <div className="background">
       <h3>Hello, world!</h3>
       <p>This is a simple hero unit</p>
       <hr />
       <p>It uses utility classes for typography and spacing to space</p>
       <Button variant="primary" style={{width : '200px'}}>Primary</Button>{' '}
     </div>
+  )
+}
+
+function Plist(props) {
+  let test = `https://codingapple1.github.io/shop/shoes${props.index + 1}.jpg`
+  return(
+      <div className="col-md-4">
+        <img src={test} alt="" srcSet="" width="100%"/>
+        <h4>{props.shoes[props.index].title}</h4>
+        <p>{props.shoes[props.index].content} & {props.shoes[props.index].price}</p>
+      </div>
   )
 }
 
