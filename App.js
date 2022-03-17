f20f876459cfee78a3f4e9fe806dceee0160f0b9
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2022-03-17 22:37:45 +0900

    #4 router 섩치와 router를 이용한 메인페이지 / 상세페이지 나누기
    #4 router 섩치와 router를 이용한 메인페이지 / 상세페이지 나누기

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
 import productData, {name1 as apple, name2} from './data.js';
 import {name3} from './data.js';
+//'./data.js'에서 .js를 생략해도 된다.
 // 설사 1개의 변수만 import 하더라도 {중괄호}는 반드시 써야한다.(안쓰면 error 발생)
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   // 상품데이터
@@ -11,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>{name3}</div>
+      
       <Navbar bg="light" expand="lg">
         <Container>
           <Navbar.Brand href="#home">Garden</Navbar.Brand>
@@ -32,17 +34,38 @@ function App() {
         </Container>
       </Navbar>
 
-      <Jumbotron/>
-
-      <div className="container">
-        <div className="row">
-      {
-        shoes.map(function(e, index) {
-          return (<Card index={index} shoes={shoes} key={index}/>);
-        })
-      }
+      <Route exact path="/">
+        {/* <div>메인페이지인데요</div> */}
+        <Jumbotron/>
+        <div className="container">
+          <div className="row">
+        {
+          shoes.map(function(e, index) {
+            return (<Card index={index} shoes={shoes} key={index}/>);
+          })
+        }
+          </div>
         </div>
-      </div>  
+      </Route>
+
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button> 
+            </div>
+          </div>
+        </div> 
+      </Route>
+
+
+      
 
     </div>
   )
@@ -61,10 +84,10 @@ function Jumbotron() {
 }
 
 function Card(props) {
-  let test = `https://codingapple1.github.io/shop/shoes${props.index + 1}.jpg`
+  let imgSrc = `https://codingapple1.github.io/shop/shoes${props.index + 1}.jpg`
   return(
       <div className="col-md-4">
-        <img src={test} alt="" srcSet="" width="100%"/>
+        <img src={imgSrc} alt="" srcSet="" width="100%"/>
         <h4>{props.shoes[props.index].title}</h4>
         <p>{props.shoes[props.index].content} & {props.shoes[props.index].price}</p>
       </div>
